829c7d003c690d84bfd624353cf3952514257c83
Moshe <moshe@pbd.co.il>
2025-06-06 01:49:10 +0300

    Fix environment variables for production build

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: true
+    allowedHosts: true,
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
-    extensions: ['.mjs', '.js', '.jsx', '.ts', '.tsx', '.json']
+    extensions: [".mjs", ".js", ".jsx", ".ts", ".tsx", ".json"],
   },
   optimizeDeps: {
     esbuildOptions: {
       loader: {
-        '.js': 'jsx',
+        ".js": "jsx",
       },
     },
   },
-}) 
+  define: {
+    "process.env.VITE_GOOGLE_CLIENT_ID": JSON.stringify(
+      process.env.VITE_GOOGLE_CLIENT_ID
+    ),
+  },
+});
